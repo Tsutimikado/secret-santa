fde315a80c4fc6fede6a845f72c2142dddb9612d
Tsutimikado <tsutimikado@git.com>
2023-12-01 17:29:00 +0500

    Изменеён фон и тени камина. Изменён телевизор

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ function App() {
   return (
     <div className="App">
       <OldTv>
-        <div class="w-full h-full bg-gradient-to-t from-red-700 via-red-700 to-[#cc2204] p-[2%]">
+        <div class="w-full h-full bg-gradient-to-r from-[#313159] via-sky-950 to-[#313159] p-[2%] relative">
+          <Fireplace />
           <div className=' w-full h-full relative' id='visible-content'>
-            <Fireplace/>
+            {/* <Fireplace/> */}
             <div id='floor-line' className="h-1 bg-red-800 absolute bottom-32 rounded-full w-11/12 left-1/2 -translate-x-1/2"></div>
           </div>
         </div>
@@ -15,5 +16,6 @@ function App() {
     </div>
   );
 }
-
+//#FE001A Coca-cola Red
+//#34a853 random
 export default App;
--- src/components/interior/Fireplace.jsx
+++ src/components/interior/Fireplace.jsx
@@ -3,20 +3,26 @@ import React from 'react'
 const Fireplace = () => {
   return (
     <div className='absolute top-0 left-1/2 -translate-x-1/2 h-[80%]'>
-        <div className="flex flex-col h-full items-center">
-           <div className='w-36 bg-amber-800  border-x-2 border-amber-900 flex-grow relative'>
-               <div className="absolute bottom-0 w-full h-24 bg-gradient-to-t from-black/50 via-transparent to-transparent"></div>
-           </div>
-           <div className='w-[22rem] bg-amber-800  border-2 border-amber-900 h-3 rounded-t-lg'></div>
-           <div className='w-[21rem] bg-amber-800  border-x-2 border-b-2 border-amber-900 h-2 rounded-b-md'></div>
-           <div className='w-80 bg-amber-800  border-x-2 border-amber-900 h-1/3 flex justify-center items-end'>
-               <div className='bg-black w-[83%] h-[87%] overflow-hidden'>
-                 <div className='bg-gradient-[radial] w-full h-full'></div>
-               </div>
-           </div>
-           <div className='w-[21rem] bg-amber-800  border-x-2 border-t-2 border-amber-900 h-2 rounded-t-lg'></div>
-           <div className='w-[25rem] bg-amber-800  border-2 border-amber-900 h-3'></div>
+      <div className="flex flex-col h-full items-center">
+        <div className='w-[70%] bg-amber-800  border-x-2 border-amber-900 flex-grow relative'>
+          <div className="absolute bottom-0 w-full h-full bg-gradient-to-t from-[#050526]/50  to-transparent"></div>
         </div>
+        {/* <div className='w-36 bg-amber-800  border-x-2 border-amber-900 flex-grow relative'>
+          <div className="absolute bottom-0 w-full h-24 bg-gradient-to-t from-black/50 via-transparent to-transparent"></div>
+        </div> */}
+        <div className="flex flex-col items-center h-1/3 relative shadow">
+          {/* <div className="absolute  w-full h-full -top-[10%] "></div> */}
+          <div className='w-[22rem] bg-amber-800  border-2 border-amber-900 h-3 rounded-t-lg '></div>
+          <div className='w-[21rem] bg-amber-800  border-x-2 border-b-2 border-amber-900 h-2 rounded-b-md '></div>
+          <div className='w-80 bg-amber-800  border-x-2 border-amber-900 flex-grow flex justify-center items-end '>
+            <div className='bg-black w-[83%] h-[87%] overflow-hidden rounded-t-md '>
+              <div className=' w-full h-full'></div>
+            </div>
+          </div>
+          <div className='w-[21rem] bg-amber-800  border-x-2 border-t-2 border-amber-900 h-2 rounded-t-lg'></div>
+          <div className='w-[25rem] bg-amber-800  border-2 border-amber-900 h-3'></div>
+        </div>
+      </div>
     </div>
   )
 }
